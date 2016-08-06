LineageOS/android_packages_apps_Settings

[1/2] Settings: Add support for OP_ALARM_WAKEUP
Port from cm-10.2 and cm-11.0

Author: Sam Mortimer <sam@mortimer.me.uk>
Date:   Mon Sep 30 23:10:35 2013 -0700
[1/2] AppOps: Add alarm wakeup op
Change-Id: Ic739519cb9df9d2c67d65e3572a119b4d55145f0

Change-Id: I417742d00f60b487942ee2adf9b75d2b04a0f766

src/com/android/settings/applications/AppOpsState.java
@@ -168,13 +168,15 @@ public class AppOpsState {
                     AppOpsManager.OP_BLUETOOTH_CHANGE,
                     AppOpsManager.OP_NFC_CHANGE,
                     AppOpsManager.OP_DATA_CONNECT_CHANGE,
-                    AppOpsManager.OP_WAKE_LOCK },
+                    AppOpsManager.OP_WAKE_LOCK,
+                    AppOpsManager.OP_ALARM_WAKEUP },
             new boolean[] { true,
                     true,
                     true,
                     true,
                     true,
-                    true }
+                    true,
+                    false }
             );
 
     public static final OpsTemplate BOOTUP_TEMPLATE = new OpsTemplate(

Naman-Bhalla/rr_packages_apps_Settings

src/com/android/settings/applications/AppOpsState.java
@@ -203,7 +203,8 @@ public class AppOpsState {
                     AppOpsManager.OP_WIFI_CHANGE,
                     AppOpsManager.OP_BLUETOOTH_CHANGE,
                     AppOpsManager.OP_NFC_CHANGE,
-                    AppOpsManager.OP_DATA_CONNECT_CHANGE },
+                    AppOpsManager.OP_DATA_CONNECT_CHANGE,
+                    AppOpsManager.OP_ALARM_WAKEUP },
             new boolean[] { false,
                     true,
                     true,
@@ -217,7 +218,8 @@ public class AppOpsState {
                     true,
                     true,
                     true,
-                    true }
+                    true,
+                    false }
             );
 
     public static final OpsTemplate BOOTUP_TEMPLATE = new OpsTemplate(